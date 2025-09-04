6304c47fd5a2a7bfbcde7b060a6eff877640a042
saffr3n <saffr3n@gmail.com>
2025-09-04 15:10:20 +0300

    feat: add ejs support and main layout partials

--- src/core/app.js
+++ src/core/app.js
@@ -1,9 +1,13 @@
 import express from 'express';
+import path from 'path';
 import session from './session.js';
 import passport from './passport.js';
 import indexRouter from '../routes/index.js';
 
 const app = express();
+
+app.set('views', path.resolve(import.meta.dirname, '../views'));
+app.set('view engine', 'ejs');
 
 app.use(session());
 app.use(passport.session());
