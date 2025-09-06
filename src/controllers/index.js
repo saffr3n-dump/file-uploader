4c05cbe3280ae143259bc8ca90ce663f622c4082
saffr3n <saffr3n@gmail.com>
2025-09-06 16:00:53 +0300

    feat: add GET /register route

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,3 +1,11 @@
 export const home = (_req, res) => {
   res.redirect('/folders');
 };
+
+export const registerGet = (req, res, next) => {
+  if (!req.isAuthenticated()) return res.render('register');
+  req.logout((err) => {
+    if (err) return next(err);
+    res.redirect('/register');
+  });
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
-import { home } from '../controllers/index.js';
+import {
+  home,
+  registerGet,
+} from '../controllers/index.js';
 
 const indexRouter = Router();
 
 indexRouter.get('/', home);
+indexRouter.get('/register', registerGet);
 
 export default indexRouter;
